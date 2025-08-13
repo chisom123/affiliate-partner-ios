f5c6cb42bc480fd443ca74fa2a30d70c7c8d108c
chisom123 <chisagu5@gmail.com>
2025-08-13 10:03:34 +0100

    Initial Commit

---
+++ SocialStar Partners/SocialStar_PartnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialStar_PartnersApp.swift
+//  SocialStar Partners
+//
+//  Created by Ezi Agu on 22/05/1404 AP.
+//
+
+import SwiftUI
+
+@main
+struct SocialStar_PartnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
